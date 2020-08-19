938fea08b968d4b0289d829a84e3a2b1d0e9f9fa
khaba <khadija.abahous@smile-maroc.com>
2020-08-19 05:04:13 -0700

    form validate and persist in DB

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Form\ContactType;
+
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -11,9 +14,19 @@ class ContactController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function index()
+    public function index(Request $request, EntityManagerInterface $entityManager)
     {
         $form = $this->createForm(ContactType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $contact = $form->getData();
+            $entityManager->persist($contact);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Merci d\'avoir pris contact');
+            return $this->redirectToRoute('home_page');
+        }
         return $this->render('contact/index.html.twig', ['contactForm' => $form->createView() ]);
     }
 
--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Contact;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,8 +14,8 @@ class ContactType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('email')
-            ->add('message')
+            ->add('email', EmailType::class)
+            ->add('message', TextareaType::class)
         ;
     }
 
